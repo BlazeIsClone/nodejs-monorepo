2efb786faef0607e2dab326f0dc7406ffbee794c
BlazeIsClone <64116737+BlazeIsClone@users.noreply.github.com>
2022-11-19 10:10:59 +0530

    package: add base styles, update app nextjs markup

--- apps/nextjs/app/layout.tsx
+++ apps/nextjs/app/layout.tsx
@@ -1,18 +1,13 @@
-import './globals.css'
+import { ReactNode } from 'react';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
-}
+import 'base-styles/global/index.scss';
+import './globals.scss';
+
+const Layout = ({ children }: { children: ReactNode }) => (
+	<html lang="en">
+		<head />
+		<body>{children}</body>
+	</html>
+);
+
+export default Layout;
--- apps/nextjs/app/page.tsx
+++ apps/nextjs/app/page.tsx
@@ -1,54 +1,25 @@
+import { Button } from 'base-elements';
 import Image from 'next/image';
-import styles from './page.module.css';
+import Link from 'next/link';
 
-export default function Home() {
-	return (
-		<div className={styles.container}>
-			<main className={styles.main}>
-				<h1 className={styles.title}>
-					Welcome to <a href="https://nextjs.org">Next.js 13!</a>
-				</h1>
+import styles from './page.module.scss';
 
-				<p className={styles.description}>
-					Get started by editing{' '}
-					<code className={styles.code}>app/page.tsx</code>
-				</p>
-
-				<div className={styles.grid}>
-					<a href="https://beta.nextjs.org/docs" className={styles.card}>
-						<h2>Documentation &rarr;</h2>
-						<p>Find in-depth information about Next.js 13</p>
-					</a>
-
-					<a
-						href="https://github.com/vercel/next.js/tree/canary/examples"
-						className={styles.card}>
-						<h2>Examples &rarr;</h2>
-						<p>Explore the Next.js 13 playground.</p>
-					</a>
-
-					<a
-						href="https://vercel.com/templates/next.js/app-directory?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-						target="_blank"
-						rel="noopener noreferrer"
-						className={styles.card}>
-						<h2>Deploy &rarr;</h2>
-						<p>Deploy your Next.js site to a public URL with Vercel.</p>
-					</a>
-				</div>
-			</main>
-
-			<footer className={styles.footer}>
-				<a
-					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer">
-					Powered by{' '}
-					<span className={styles.logo}>
-						<Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-					</span>
-				</a>
-			</footer>
+const Home = () => (
+	<div className={styles.main}>
+		<div>
+			<Image
+				className={styles.logo}
+				src={'/nextjs.svg'}
+				alt={'NextJS logo'}
+				width={'805'}
+				height={'650'}
+			/>
 		</div>
-	);
-}
+		<h1>NextJS 13 + React 18</h1>
+		<Link href="https://beta.nextjs.org/docs" target={'_blank'}>
+			<Button type="contained">Read Docs</Button>
+		</Link>
+	</div>
+);
+
+export default Home;
